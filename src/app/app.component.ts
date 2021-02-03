923e16c0155e78baa93d3f288f46aa73904f24a3
Guru Mulay <guruprasadmulay@gmail.com>
2021-02-03 23:27:31 +1100

    Initial commit
    - Intialised angular project
    - Rendering weather for a week
    
    - Used tailwind css, lodash, moment js
    
    - Added weather component to display a weather object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sleep-fit';
+export class AppComponent implements OnInit {
+  title = 'Sleep Fit';
+  weathers:any;
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    console.log('call axios');
+    this.getWeathers();
+  }
+
+  getWeathers(): void {
+    console.log('called');
+    this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=sydney&appid=311f955af54e5967569d27adde85d4c4')
+      .toPromise()
+      .then((res: any) => {
+        if (res) {
+          const todayDate = moment().format('DD/MM/YYYY');
+          const list = _.map(res.list, (el) => {
+            const currentDatetime = moment(el.dt_txt);
+            const formattedDate = currentDatetime.format('DD/MM/YYYY');
+            // const formattedDate = currentDatetime.getDate() +
+            //   '-' +
+            //   (currentDatetime.getMonth() + 1) +
+            //   '-' +
+            //   currentDatetime.getFullYear();
+            console.log(formattedDate);
+            return {
+                ...el,
+                date: formattedDate,
+                today: formattedDate === todayDate
+              };
+            });
+          console.log(list, _.groupBy(list, 'date'));
+          this.weathers = _.map(_.groupBy(list, 'date'), (el, key) => {
+            console.log(el, key);
+            return {
+              date: moment(key, 'DD/MM/YYYY').format('DD MMM YYYY (ddd)'),
+              today: key === todayDate,
+              values: el
+            };
+          });
+          console.log(this.weathers);
+        }
+      });
+  }
 }
